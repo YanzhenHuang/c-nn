6d2911cbec20f476b006af6e4c3c7fc8cbdf85fb
yanzhen huang <huangyanzhen0108@163.com>
2024-12-25 20:48:17 +0800

    :bee: Update printf code fragments in xlinalg.c.

--- xlinalg.c
+++ xlinalg.c
@@ -200,7 +200,8 @@ Matrix* xmat_solve(Matrix* A, Matrix* b){
         // Check vor zero-pivot.
         double pivot = mat_read(hybrid_mat, i, i);
         if (pivot == 0){
-            printf("Solve equation failed: Pivot element is zero at row %lld during backward substitution.", i);
+            printf("Solve equation failed: Pivot element is zero at"
+                    " row %lld during backward substitution.", i);
             exit(1);
         }
 
